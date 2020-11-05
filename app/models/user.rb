b875babc0388637a9af9402dfb04f31448ddf7f0
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-05 20:52:34 +0900

    ユーザー管理機能3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     validates :password,format: {with: /\A[a-zA-Z0-9]+\z/}
+  
+  character_style  =  /\A[ぁ-んァ-ン一-龥]/
+  character = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
     validates :nickname
-    validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name_kana,format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :last_name_kana,format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name,format: { with: character_style, message: "is invalid. Input full-width characters."}
+    validates :last_name,format: { with: character_style, message: "is invalid. Input full-width characters."}
+    validates :first_name_kana,format: { with: character, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_kana,format: { with: character, message: "is invalid. Input full-width katakana characters."}
     validates :birth_date
   end
 end
