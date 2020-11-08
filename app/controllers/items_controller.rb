1b63251b32ed654fa3b885913a8951e5531a9be9
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-08 21:47:55 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,28 @@
 class ItemsController < ApplicationController
-# before_action :move_to_index, except: [:index, :show]
+#before_action :move_to_index, except: [:index, :show]
   
   def index
   end
 
   def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to root_path
+    end
   end
 
-  # def create
-  #   @item = Item.new(item_params)
-  #   if @item.save
-  #     redirect_to root_path      
-  #   else
-  #     render :new  
-  #   end
-  # end
+  def create
+     @item = Item.new(item_params)
+     if @item.save
+      redirect_to root_path      
+     else
+      render :new
+     end
+  end
   
-  # def move_to_index
-  #   unless user_signed_in?
-  #     redirect_to action: :index
-  #   end
-  # end
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  before do
+  RSpec.describe User, type: :model do
+    before do
       @user = FactoryBot.build(:user)
-  end
+    end
 
     describe 'ユーザー新規登録' do
       context '新規登録がうまくいくとき' do
         it "全部の項目が存在すれば登録できる" do
           expect(@user).to be_valid
         end
+      end
 
-     context '新規登録がうまくいかないとき' do
+      context '新規登録がうまくいかないとき' do
         it "nicknameが空だと登録できない" do
           @user.nickname = ""
           @user.valid?
@@ -133,7 +134,6 @@ RSpec.describe User, type: :model do
       end
     end  
   end
-end
 
 
   
