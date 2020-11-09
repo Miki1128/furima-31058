963efc12824e67cf979857190295ea803c47e30a
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-10 02:04:42 +0900

    商品一覧表示機能2

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,21 +8,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_day
   belongs_to_active_hash :status
   belongs_to_active_hash :category
-  
-  line = {other_than: 0 }
- 
+
+  line = { other_than: 0 }
+
   with_options presence: true do
     validates :image
     validates :name
     validates :description
-    validates :category_id, numericality: {with: line, message:  "can't be blank"}
-    validates :status_id, numericality: {with: line, message:  "can't be blank"}
-    validates :shipping_cost_id, numericality: {with: line, message:  "can't be blank"}
-    validates :prefecture_id , numericality: {with: line, message:  "can't be blank"}
-    validates :shipping_day_id, numericality: {with: line, message:  "can't be blank"}
-    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000000, message: "is out of setting range"}
+    validates :category_id, numericality: { with: line, message: "can't be blank" }
+    validates :status_id, numericality: { with: line, message: "can't be blank" }
+    validates :shipping_cost_id, numericality: { with: line, message: "can't be blank" }
+    validates :prefecture_id, numericality: { with: line, message: "can't be blank" }
+    validates :shipping_day_id, numericality: { with: line, message: "can't be blank" }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1_000_000, message: 'is out of setting range' }
   end
 end
-
-
