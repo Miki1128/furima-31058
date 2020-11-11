232e9447cf3a2b63e026f09a7771584761313852
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-11 22:28:06 +0900

    商品情報編集機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,18 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to root_path
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
@@ -40,5 +37,9 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,18 +9,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :category
 
-  line = { other_than: 0 }
-
   with_options presence: true do
     validates :image
     validates :name
     validates :description
-    validates :category_id, numericality: { with: line, message: "can't be blank" }
-    validates :status_id, numericality: { with: line, message: "can't be blank" }
-    validates :shipping_cost_id, numericality: { with: line, message: "can't be blank" }
-    validates :prefecture_id, numericality: { with: line, message: "can't be blank" }
-    validates :shipping_day_id, numericality: { with: line, message: "can't be blank" }
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1_000_000, message: 'is out of setting range' }
+
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_cost_id
+      validates :prefecture_id
+      validates :shipping_day_id
+      validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
+      validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1_000_000, message: 'is out of setting range' }
+    end
   end
 end
