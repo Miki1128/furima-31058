2da378a9d8a5a05f46cc4d0a096ad71f237358de
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-12 15:12:07 +0900

    商品削除機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -30,9 +30,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
     if @item.user == current_user
+      @item.destroy
       redirect_to root_path
+    else
+      render :show
     end
   end
 
