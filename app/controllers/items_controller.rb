ee798e3269ed5b2b116b747cffdb24dce8b1488b
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-12 15:21:59 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,12 +30,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user == current_user
-      @item.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
