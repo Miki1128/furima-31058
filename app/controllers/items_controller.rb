8e4678a30c62f9aa6021e1902bcb966765749082
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-17 21:54:34 +0900

    商品購入機能6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id != @item.user.id
       redirect_to action: :index 
     end
